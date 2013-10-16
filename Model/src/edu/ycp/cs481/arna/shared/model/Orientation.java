550cb1c542e4a2b9a02c9bd4a73ede4e0aab6c8f
Donny <dandrew2@ycp.edu>
2013-10-16 12:47:03 -0400

    Added Orientation class

--- Model/src/edu/ycp/cs481/arna/shared/model/Orientation.java
+++ Model/src/edu/ycp/cs481/arna/shared/model/Orientation.java
@@ -2,6 +2,38 @@ package edu.ycp.cs481.arna.shared.model;
 
 public class Orientation {
 	private float azimuth; 
-	private float elevation; 
-	 
+	private float pitch;
+	private float roll; 
+	
+	public Orientation(float az, float pitch, float roll){
+		this.azimuth = az; 
+		this.pitch = pitch; 
+		this.roll = roll;
+	}
+
+	public float getAzimuth() {
+		return azimuth;
+	}
+
+	public void setAzimuth(float azimuth) {
+		this.azimuth = azimuth;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+	public void setPitch(float pitch) {
+		this.pitch = pitch;
+	}
+
+	public float getRoll() {
+		return roll;
+	}
+
+	public void setRoll(float roll) {
+		this.roll = roll;
+	}
+	
+	
 }
