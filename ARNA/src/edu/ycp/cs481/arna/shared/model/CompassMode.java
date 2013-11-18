13706da5cbefa36e917c5d4e7a9e045622f62058
ccampagn <ccampagn@ycp.edu>
2013-11-17 20:55:50 -0500

    Adding things for FIFO in Compass Mode

--- ARNA/src/edu/ycp/cs481/arna/shared/model/CompassMode.java
+++ ARNA/src/edu/ycp/cs481/arna/shared/model/CompassMode.java
@@ -1,16 +1,26 @@
 package edu.ycp.cs481.arna.shared.model;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 public class CompassMode {
 	
 	private POI destination;
 	private double direction;
 	private double distance;
 	private User user;
+	private ArrayList<Integer> fifo;
+	private int sum,limit;
+	private double average;
 	
 	public CompassMode(User u, POI w) {
 		 user = u;
 		 destination = w;
 		 direction = 0;
+		 fifo = new ArrayList<Integer>();
+		 sum=0;
+		 limit=10;
+		
 	}
 	
 	
@@ -45,4 +55,49 @@ public class CompassMode {
 	public void setDirection(double drctn) {
 		direction = drctn; 
 	}
-}
+	public int getsum(){
+		return sum;	
+	}
+	public void setsum(int sum){
+		this.sum=sum;	
+	}
+	public void calcsum(){
+		int sum=0;
+		for	(int x=0;x<getlimit();x++){
+			sum=fifo.get(x)+sum;
+		}
+		setsum(sum);
+	}
+	public double getaverage(){
+		return average;	
+	}
+	public void setaverage(double average){
+		this.average=average;	
+	}
+	public void calcaverage(){
+		calcsum();
+		double aver=(double)getsum()/fifo.size();
+		setaverage(aver);
+	}
+	public int getlimit(){
+		return limit;	
+	}
+	public boolean fullfifo(){
+		if(fifo.size()==limit){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public void addvalue(int value){
+		if(fullfifo()==true){
+			fifo.remove(0);
+			fifo.add(value);
+		}else{
+			fifo.add(value);
+		}
+		}
+	}
+	
+
+
